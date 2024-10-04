27acf229d4cbeebca57c797389173598e64b0a95
THIRU2308 <161467239+THIRU2308@users.noreply.github.com>
2024-10-04 11:13:37 +0530

    Add files via upload

---
+++ JS/prb4.js
@@ -0,0 +1,19 @@
+let boxes=prompt("Enter the number separated by commas");
+let arr=boxes.split(",").map(Number);
+let box=[];
+let emptyboxcount=0;
+let i;
+for(i=0;i<arr.length;i++){
+    if(arr[i]==0){
+        emptyboxcount++;
+    }
+    else{
+        box.push(arr[i]);
+    }
+}
+box=box.sort();
+for(i=0;i<emptyboxcount;i++){
+    box.push(0);
+}
+console.log(box);
+console.log(emptyboxcount);
